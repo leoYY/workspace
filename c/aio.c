679549ab15fc7b99375e02f6fdc2a3c829df0a4c
yuanyi <yuanyi@raspberrypi.(none)>
2013-07-23 15:45:22 +0000

    aio commmit

---
+++ c/aio.c
@@ -0,0 +1,142 @@
+// Copyright (c) 2013 doodod.com , Inc. All Rights Reserved
+// @author: yuanyi (yuan.yi.coding@gmail.com)
+// @brief : 
+
+#include "aio.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+static int processEvent(event_driver_t* driver)
+{
+    if (driver == NULL)        
+        return RET_ERR;
+    if (driver->maxfd <= 0 ||driver->length <= 0)  
+}
+
+int process(event_driver_t* driver)
+{
+    if(driver == NULL)
+        return RET_ERR;
+
+    while(driver->stop != 0)     
+    {
+        if (driver->beforesleep != NULL &&
+               driver->beforesleep(driver) != RET_SUC)     
+            break;
+        if (processEvent(driver) != RET_SUC)             
+            break;
+    }
+    return RET_ERR;
+}
+
+evnet_t* getEventByFd(event_driver_t* driver, int fd)
+{
+    if (driver == NULL ||
+            driver->maxfd < fd )
+        // fd wanted not exists.
+        return NULL;
+
+    if (driver->events[fd]->fd == 0)
+        return NULL;
+
+    return driver->events[fd];
+}
+
+int registEvent(event_driver_t* driver, int fd, void *clientData, int mask)
+{
+    if (fd <= 0 || driver == NULL)
+        return RET_ERR;
+
+    if (mask < 0 || mask >AE_ALL)
+        return RET_ERR;
+
+    if (fd > length)
+        return RET_ERR;
+
+    if (driver->events[fd] == NULL)
+        return RET_ERR;
+
+    event_t* event = driver->events[fd] ;
+    // check if fd is already registed.
+    if (event->fd != 0 && 
+            (event->mask == mask || event->mask == AE_ALL))
+        return RET_ERR;
+
+    event->fd = fd;
+    event->clientData = clientData;
+    event->mask |= mask;
+    if (driver->maxfd < fd)
+        driver->maxfd = fd;
+    return RET_SUC;
+}
+
+int unregistEvent(event_driver_t* driver, int fd, int delmask)
+{
+    if (driver == NULL || fd <= 0 )
+        return RET_ERR;
+
+    if (fd > driver->maxfd || 
+            driver->events[fd] == 0)
+        // not exists
+        return RET_ERR
+        
+    event_t* event = driver->events[fd];
+    if (event->mask & delmask == 0)
+        // mask to del isn't exists
+        return RET_ERR;
+
+    event->mask ^= delmask;
+    return RET_SUC;    
+}
+
+int createEventDriver(event_driver_t* driver, size_t size)
+{
+    if ( size <= 0)
+        return RET_ERR;
+
+    driver = (event_driver_t*) malloc(sizeof(event_driver_t));
+    if (driver == NULL)
+        return RET_ERR;
+    memset(driver, 0, sizeof(event_driver_t));
+
+    do {
+    
+        driver->events = (event_t*) malloc(sizeof(event_t)*size);
+        if (driver->events == NULL)
+            break;
+        
+        driver->fireds = (event_t*) malloc(sizeof(event_t)*size);
+        if (driver->fireds == NULL)
+            break;
+
+        driver->length = size;
+        return RET_SUC;
+    }while (0)
+     
+    // do some failover
+    if (driver->fireds != NULL)
+        free(driver->fireds);
+
+    if (driver->events != NULL)
+        free(driver->events);
+
+    if (driver != NULL)
+        free(driver);
+
+    return RET_ERR;
+}
+
+int destroyEventDrivert(event_driver_t* driver)
+{
+    if (driver->fireds != NULL)
+        free(driver->fireds);
+
+    if (driver->events != NULL)
+        free(driver->events);
+
+    if (driver != NULL)
+        free(driver);
+
+    return RET_SUC;
+}
---
+++ c/aio.h
@@ -0,0 +1,62 @@
+// Copyright (c) 2013 doodod.com , Inc. All Rights Reserved
+// @author: yuanyi (yuan.yi.coding@gmail.com)
+// @brief :  event-driver lib
+
+#ifndef _AIO_H_
+#define _AIO_H_
+
+typedef struct _event_driver event_driver_t;
+// proc
+typedef int eventProc(event_driver_t* driver, int fd, void* clientData, int mask);
+typedef int eventFinalizer(event_driver_t* driver, void *clientData);
+typedef int eventBeforeSleepProc(event_driver_t* driver);
+
+typedef struct _event {
+    int mask;
+    int fd;
+    eventProc* readProc;
+    eventProc* writeProc;
+    void* clientData;
+} event_t;
+
+typedef struct _event_driver {
+    int maxfd; 
+    size_t length;
+    int stop;
+    event_t* events;
+    event_t* fireds;
+    eventFinalizer* finalizer;
+    eventBeforeSleepProc* beforesleep;
+} event_driver_t;
+
+typedef enum _event_status {
+    AE_NONE = 0,
+    AE_READABLE,
+    AE_WRITEBLE,
+    AE_ALL;
+} event_status_t;
+
+typedef enum _ret_status {
+    RET_SUC = 0,
+    RET_ERR;
+} ret_status_t;
+
+int createEventDriver(event_driver_t* driver, size_t size);
+int destroyEventDrivert(event_driver_t* driver);
+int registEvent(event_driver_t* driver, int fd, void* clientData, int mask);
+int unregistEvent(event_driver_t* driver, int fd, int delmask);
+event_t* getEventByFd(event_driver_t* driver, int fd);
+int process(event_driver_t* driver);
+
+
+// API
+extern void event_API_addEvent(evnet_driver_t* driver, int fd, int mask);
+extern void event_API_delEvent(evnet_driver_t* driver, int fd, int delmask);
+extern void event_API_poll(evnet_driver_t* driver, struct timeval *tvp);
+extern void event_API_free(event_driver_t* driver);
+extern void event_API_resize(event_driver_t* driver);
+extern void event_API_init(event_driver_t* driver);
+extern void event_API_name(void);
+
+#endif
+
