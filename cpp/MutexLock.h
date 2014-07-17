7b638b4a77e10809c32b6bee0a9d512f62242b19
leo <yuan.yi.coding@gmail.com>
2014-07-18 00:15:11 +0800

    create mutexlock

---
+++ cpp/MutexLock.h
@@ -0,0 +1,14 @@
+
+#include <pthread.h>
+
+namespace common {
+namespace thread {
+
+class Mutex: public boost::noncopyable
+{
+public :
+    
+};
+
+}   // ending namespace thread
+}   // ending namespace common
